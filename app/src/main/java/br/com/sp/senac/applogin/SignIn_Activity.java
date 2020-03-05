cbb0c3cdf235309eb6da53b05a9f08a79987b3d8
guilherme.nsantos7 <guilherme_nsantos2002@hotmail.com>
2020-03-05 12:21:58 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/applogin/SignIn_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.applogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_in_layout);
+    }
+}
